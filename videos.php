b1e33c737b28b1a0611bdb72cfaeb92f5f35b4aa
Gabrielsb1 <joaogabrielmoto2015@gmail.com>
2024-11-29 17:48:08 -0300

    first commit

---
+++ videos.php
@@ -0,0 +1,37 @@
+<?php
+
+function limpanome($path,$nomecomextensao){
+	
+	$nomec = pathinfo($path.$nomecomextensao, PATHINFO_FILENAME);
+	$ext   = pathinfo($path.$nomecomextensao, PATHINFO_EXTENSION);
+	
+	$string = iconv( "UTF-8" , "ASCII//TRANSLIT//IGNORE" , $nomec );
+	$string = preg_replace( array( '/[ ]/' , '/[^A-Za-z0-9\-]/' ) , array( '' , '' ) , $string );
+	
+	rename($path.$nomecomextensao, $path.$string.".".$ext);
+	
+	return $string.".".$ext;
+	
+}
+
+$path = "themes/cartorio/video/";
+
+$files = scandir($path);
+$arrayJson = array();
+foreach($files as $key=>$f){
+	if(($f!='.') && ($f!='..')){		
+		$arrayJson[] = limpanome($path,$f);		
+	}
+}
+/*
+$arrayJson = array(
+ 	'escriturapublica.mp4',
+	'autenticacao.mp4',    	
+	'autorizacaoviagem.mp4',
+	'emancipacao.mp4',
+);
+//codificar para Json (isso será passado para o Javascript)
+*/
+echo json_encode($arrayJson);
+
+?>
